14dba227e64894b56ee3aa9dfd45d064239a1b05
Sonam Shah <sonam.shah1687@gmail.com>
2021-01-13 15:33:40 -0600

    Added user validations,

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false }, 
+                       length: { minimum: 3, maximum: 25 }  
+  VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/                    
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false },  
+                    length: { minimum: 3, maximum: 105 },
+                    format: { with: VALID_REGEX }
 	has_many :articles
 end
